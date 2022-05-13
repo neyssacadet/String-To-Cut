246ae6d4f731b0e2e793c9ae5c138e966d1f0e74
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-05-13 18:00:03 -0500

    created and tested cutString function.

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const app = express();
 
+function cutString (stringInput){
+    let processedString = '';
+    const characters = stringInput.split('');
+    for (let i = 2; i < characters.length; i += 3) {
+      processedString += characters[i];
+    }
+    
+    return { return_string: processedString };
+}
+console.log(cutString("iamyourlyftdriver"));
 
+//Post route
 app.post('/test', (req, res) => {
     res.send('POST request to the homepage')
   })
